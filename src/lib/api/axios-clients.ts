45ba063807243668eb7f0699b97805351d9fb8b3
arunpragash-ap <arunpragash46@gmail.com>
2025-08-01 22:30:25 +0530

    Make the api requests are goes to the application with /api

--- src/lib/api/axios-clients.ts
+++ src/lib/api/axios-clients.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API_BASE_URL =
-  process.env.NEXT_PUBLIC_API_BASE_URL || "http://192.168.1.100:5000/";
+  process.env.NEXT_PUBLIC_API_BASE_URL || "http://192.168.1.100:5000/api/";
 
 export const axiosAuth = axios.create({
   baseURL: API_BASE_URL,
